c54224fa76f92ec5fa4495bfa1237b4c2e78c6f4
Robert Meissner <robert.meissner@codecentric.de>
2022-05-17 09:55:36 +0200

    WIP
    Fixing shell scripts

--- runService.sh
+++ runService.sh
@@ -1,1 +1,1 @@
-docker run  -p 8080:8080 -d --name wlo-topic-assistant -v `pwd`/src:/scr wlo-topic-py python3 /scr/webservice.py 
+docker run  -p 8080:8080 -d --name wlo-topic-assistant -v `pwd`/src:/scr wlo-topics-py python3 /scr/webservice.py
